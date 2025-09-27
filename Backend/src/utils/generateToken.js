b1b92e5681fc31124dcad07f81c9c441d8a08d5c
saugat1070 <saugatgiri1070@gmail.com>
2025-09-28 05:38:39 +0545

    setup cookies with jwt

---
+++ Backend/src/utils/generateToken.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+import { envConfig } from "../config/envConfig.js";
+
+export const generateToken = (userId,res)=>{
+    const token = jwt.sign({user : userId},envConfig.jwt_secret,{expiresIn : "7d"});
+    res.cookie("jwt",token,{
+        maxAge : 7*24*60*60*1000,//ms
+        httpOnly : true,  //prevent xss attacks: cross-site scripting
+        sameSite : "strict", //csrf attacks
+        secure : envConfig.nodeEnv == "production" ? true : false
+    });
+    return token;
+}
