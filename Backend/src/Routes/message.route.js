97a459c49fdb218bcd9b1aad655997d3cd4418e5
saugat1070 <saugatgiri1070@gmail.com>
2025-10-08 00:20:42 +0545

    route for message

--- Backend/src/Routes/message.route.js
+++ Backend/src/Routes/message.route.js
@@ -1,15 +1,15 @@
 import express from "express";
 import messageController from "../controller/message.controller.js";
-
+import { protectedRoute } from "../middleware/auth.middleware.js";
 const messageRouter = express.Router();
 
 messageRouter.get("/", (req, res) => {
     res.json({ message: "Message route working" });
 });
 
-messageRouter.route("/contacts").get(messageController.getAllContacts);
-messageRouter.route("/chats").get(messageController.getAllChatsPartner);
-messageRouter.route("/:id")/get(messageController.getMessage);
-messageRouter.route("/send/:id").post(messageController.sendMessage)
+messageRouter.route("/contacts").get(protectedRoute,messageController.getAllContacts);
+messageRouter.route("/chats").get(protectedRoute,messageController.getAllChatsPartner);
+messageRouter.route("/:id").get(protectedRoute,messageController.getMessage);
+messageRouter.route("/send/:id").post(protectedRoute,messageController.sendMessage)
 
 export default messageRouter;
