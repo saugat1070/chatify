933f229d50cf4900f02a84b1172675193141513c
saugat1070 <saugatgiri1070@gmail.com>
2025-10-19 23:28:37 +0545

    setup for chatpage

--- fronted/src/pages/chatPage.jsx
+++ fronted/src/pages/chatPage.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
-import { useAuthStore } from '../Store/userAuth';
-
+import { useChatStore } from '../Store/useChatStore';
+import BorderAnimatedContainer from '../components/BorderAnimated';
+import ProfileHeader from '../components/ProfileHeader';
+import ActiveTabSwitch from '../components/ActiveTabSwitch';
+import ChatList from '../components/ChatList';
+import ContactList from '../components/ContactList';
 
 
 function ChatPage() {
-  const { logout } = useAuthStore();
-
-  const handleLogout = () => {
-    logout();
-  }
-
+  const {activeTab} = useChatStore();
   return (
-    <div>
-      <button onClick={handleLogout} className="relative bg-blue-500 text-white border-gray-700 px-4 py-2 rounded">
-        Log out
-      </button>
+    <div className='relative w-full max-w-6xl h-[800px]'>
+      <BorderAnimatedContainer>
+        {/* Left side */}
+        <div className='w-80 bg-slate-800 backdrop-blur-sm flex flex-col'>
+          <ProfileHeader/>
+          <ActiveTabSwitch/>
+          <div
+          className='flex-1 overflow-y-auto p-4 space-y-2'
+          >
+            {activeTab === "chats" ? <ChatsList/> : <ContactList/>}
+          </div>
+        </div>
+      </BorderAnimatedContainer>
     </div>
   )
 }
 
-export default ChatPage
+export default ChatPage;
 
