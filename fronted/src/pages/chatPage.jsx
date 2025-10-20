10742f830b2a62a343a27c89b83b86206908ae1c
saugat1070 <saugatgiri1070@gmail.com>
2025-10-20 22:06:49 +0545

    hehe

--- fronted/src/pages/chatPage.jsx
+++ fronted/src/pages/chatPage.jsx
@@ -19,7 +19,7 @@ function ChatPage() {
           <div
           className='flex-1 overflow-y-auto p-4 space-y-2'
           >
-            {activeTab === "chats" ? <ChatsList/> : <ContactList/>}
+            {activeTab === "chats" ? <ChatList/> : <ContactList/>}
           </div>
         </div>
       </BorderAnimatedContainer>
