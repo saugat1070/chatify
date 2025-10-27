00bf7d46bd30905e6d235a173738c45a2c7c071b
saugat1070 <saugatgiri1070@gmail.com>
2025-10-27 13:37:10 +0545

    working on chatlist

--- fronted/src/components/ChatList.jsx
+++ fronted/src/components/ChatList.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
-
+import { useEffect } from "react";
+import { useChatStore } from "../Store/useChatStore"
+import {UsersLoadingSkeleton} from "./UserLoadingSkeleton.jsx"
 export default function ChatList() {
+  const {getMyChatPartner,chats,isUserLoading,setSelectedUser} = useChatStore();
+  useEffect(()=>{
+    getMyChatPartner()
+  },[getMyChatPartner]);
+  if(isUserLoading) return <UsersLoadingSkeleton/>
   return (
-    <div>ChatList</div>
+    <>
+    {
+      chats.map((chat)=>{
+        return <div
+        key={chat?._id}
+        className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20"
+        onClick={()=>setSelectedUser(chat)}
+        >
+          <div className="flex items-center gap-3">
+            <div className="avatar avatar-online">
+              <div className="size-12 rounded-full">
+                <img src={chat.profilePic || "/avatar.png"} alt={chat.name} />
+              </div>
+            </div>
+            <h4 className="text-slate-200 font-medium truncate">{chat.name}</h4>
+          </div>
+        </div>
+      })
+    }
+    </>
   )
 }
