1dc10e70789e206c932409041cb8385a98fcae5b
saugat1070 <saugatgiri1070@gmail.com>
2025-11-11 15:51:50 +0545

    adding all files

--- fronted/src/components/ChatContainer.jsx
+++ fronted/src/components/ChatContainer.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChatStore } from '../Store/useChatStore'
+import { useAuthStore } from '../Store/userAuth';
+import ChatHeader from './ChatHeader';
+import NoChatHistory from './NoChatHistory';
+import MessageInput from './MessageInput';
+import { MessagesLoadingSkeleton } from './MessagesLoadingSkeleton';
 
 export default function ChatContainer() {
+  const {selectedUser,getMessagesByUserId,messages,isMessagesLoading} = useChatStore();
+  const {authUser} = useAuthStore();
+
+  useEffect(()=>{
+    getMessagesByUserId(selectedUser?._id);
+  },[selectedUser,getMessagesByUserId]);
+  console.log(messages)
   return (
-    <div>ChatContainer</div>
-  )
+    <>
+    <ChatHeader/>
+    <div className='flex-1 px-6 overflow-y-auto py-8'>
+      {messages && messages.length > 0 && !isMessagesLoading ? (
+        <div className='max-w-3xl mx-auto space-y-6'>
+          {messages.map((message)=>(
+            <div
+            key={message._id}
+            className={`chat ${message.senderId === authUser?._id ? "chat-end" : "chat-start"}`}
+            >
+              <div
+              className={`chat-bubble relative ${message?.senderId === authUser?._id ? "bg-cyan-600 text-white" : "bg-slate-800 text-slate-200" }`}
+              >
+                {message.img && (
+                  <img src={message.image} alt='shared' className='rounded-lg h-48 object-cover'/>
+                      )}
+                {message.text && (<p className='mt-2 p-2'>{message.text}</p>)}
+                <p className='text-xs mt-1 opacity-75 flex items-center gap-1'>
+                  {new Date(message?.createdAt).toISOString().slice(11,16)}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : isMessagesLoading ? (<MessagesLoadingSkeleton/>) : (<NoChatHistory name={selectedUser?.name}/>)}
+    </div>
+    <MessageInput/>
+    </>
+    )
 }
--- fronted/src/components/ContactList.jsx
+++ fronted/src/components/ContactList.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useChatStore } from '../Store/useChatStore'
+import { UsersLoadingSkeleton } from './UserLoadingSkeleton';
 export default function ContactList() {
+
+  const {isUserLoading,getAllContacts,allContacts,setSelectedUser} = useChatStore();
+  useEffect(()=>{
+    getAllContacts();
+  },[getAllContacts])
+  if(isUserLoading) return <UsersLoadingSkeleton/>
   return (
-    <div>ContactList</div>
+    <>
+    {
+      allContacts.map((contact)=>{
+        return <div
+        key={contact?._id}
+        className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20"
+        onClick={()=>setSelectedUser(contact)}
+        >
+          <div className="flex items-center gap-3">
+            <div className="avatar avatar-online">
+              <div className="size-12 rounded-full">
+                <img src={contact.profilePic || "/avatar.png"} alt={contact.name} />
+              </div>
+            </div>
+            <h4 className="text-slate-200 font-medium truncate">{contact.name}</h4>
+          </div>
+        </div>
+      })
+    }
+    </>
   )
 }
